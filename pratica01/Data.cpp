7df146679685e790ac2fe1c7db4fc9398d0173ba
matheusximeness <matheus.ximenes2016@outlook.com>
2020-03-04 20:59:03 -0300

    complementando implemetação

--- pratica01/Data.cpp
+++ pratica01/Data.cpp
@@ -54,14 +54,34 @@ int Data::difDias(const Data& d){
 }
 
 void Data::setDia(int d){
+    if(d<1 || d>30){
+        if(d<1)
+            d = 1;
+        if(d>30)
+            d = 30;
+    }
     dia = d;
 }
 
 void Data::setMes(int m){
+    if(m<1 || m>12){
+        if(m<1)
+            m = 1;
+        if(m>12)
+            m = 30;
+    }
+
     mes = m;
 }
 
 void Data::setAno(int a){
+    if(a<2018 || a>2020){
+        if(a<2018)
+            a = 2018;
+        if(a>2020)
+            a = 2020;
+    }
+
     ano = a;
 }
 
@@ -78,5 +98,27 @@ int Data::getAno() const {
 }
 
 void Data::imprime() {
-    cout << dia << "/" << mes << "/" << ano << endl;
+    	cout << getDia() << "/" << getMes() << "/" << getAno() << endl;
+}
+
+ostream & operator<<(ostream &cout, const Data &dt){
+	dt.imprime();
+	
+	return cout;
+}
+
+istream & operator>>(istream &cin, Data &dt){
+	dt.le();
+	
+	return cin;
+}
+
+void Data::le(){
+	int d,m,a;
+	cout << "dia: ";
+	cin >> d; setDia(d);
+	cout << "mes: ";
+	cin >> m; setMes(m);
+	cout << "ano: ";
+	cin >> a; setAno(a);
 }
