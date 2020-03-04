2177b5bb4253ac94bd1cac37bb5f91e61e9e2801
Matheus Ximenes <matheus.ximenes2016@outlook.com>
2020-03-04 18:41:48 -0300

    implementado

---
+++ pratica01/Agenda.h
@@ -0,0 +1,27 @@
+#ifndef AGENDA_H
+#define AGENDA_H
+
+class ItemAgenda {
+    public:
+        ItemAgenda();
+        ItemAgenda(const string &, const Data &, const Horario &);
+        ItemAgenda(const ItemAgenda &);
+        void setDesc(const string &);
+        void setData(const Data &);
+        void setHorario(const Horario &);
+        string getDesc () const;
+        Data getData() const;
+        Horario getHorario() const;
+
+    private:
+        string descricao; 
+        Data d;
+        Horario h;
+};
+
+class Agenda {
+    public:
+        Agenda();
+        insereItem(const ItemAgenda &);
+        compromissosData(const Data &);
+};
