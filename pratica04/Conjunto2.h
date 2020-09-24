aaf562977100589286fb4274b217f45272291a44
matheusximeness <matheus.ximenes2016@outlook.com>
2020-09-24 15:31:34 -0300

    etapa 2 fixing

---
+++ pratica04/Conjunto2.h
@@ -0,0 +1,18 @@
+#ifndef CONJUNTO2_H
+#define CONJUNTO2_H
+#include<iostream>
+#include<istream>
+#include<ostream>
+using namespace std;
+
+template<class T>
+class Conjunto;
+
+template<class T>
+class Conjunto {
+    public:
+    
+    private:
+};
+
+#endif
