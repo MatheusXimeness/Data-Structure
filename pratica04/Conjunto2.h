b6d6992b558a9706c75e317a545d49f08e98dace
matheusximeness <matheus.ximenes2016@outlook.com>
2020-09-25 14:35:17 -0300

    etapa 2 finish

--- pratica04/Conjunto2.h
+++ pratica04/Conjunto2.h
@@ -34,7 +34,6 @@ class Conjunto {
         bool operator==(const Conjunto<T>&) const;
         const Conjunto &operator=(const Conjunto<T>&);
 
-        void le( Conjunto<T>);
         void imprime() const;
     private:
         T *c;
@@ -85,7 +84,7 @@ bool Conjunto<T>::insere(const T &elem){
 
 template<class T>
 bool Conjunto<T>::pertence(const T &elem) const {
-    for(int i=0;i<tamArray;i++){
+    for(int i=0;i<num_elementos;i++){
         if(c[i] == elem){
             return true;
         }
@@ -95,9 +94,9 @@ bool Conjunto<T>::pertence(const T &elem) const {
 
 template<class T>
 bool Conjunto<T>::operator==(const Conjunto<T>&other) const{
-    if(tamArray != other.tamArray)
+    if(num_elementos != other.num_elementos)
         return false;
-    for(int i=0;i<tamArray;i++)
+    for(int i=0;i<num_elementos;i++)
         if(!pertence(other.c[i]))
             return false;
     return true;
@@ -114,7 +113,7 @@ const Conjunto<T> &Conjunto<T>::operator=(const Conjunto<T>&other){
     tamArray = other.tamArray;
     num_elementos = other.num_elementos;
     c = new T[tamArray];
-    for(int i=0; i<tamArray; i++) c[i] = other.c[i];    
+    for(int i=0; i<other.num_elementos;i++) insere(other.c[i]);    
     return *this;
 }
 
@@ -123,44 +122,48 @@ Conjunto<T> Conjunto<T>::operator+(const Conjunto<T>&b) const{
     int tam = tamArray + b.tamArray;
     Conjunto<T> temp(tam);
 
-    for(int i = 0;i<tamArray;i++)temp.insere(c[i]);
-    for(int i = 0;i<b.tamArray;i++) temp.insere(b.c[i]);
+    for(int i = 0;i<num_elementos;i++)temp.insere(c[i]);
+    for(int i = 0;i<b.num_elementos;i++) temp.insere(b.c[i]);
 
     return temp;
 }
 
 template<class T>
 Conjunto<T> Conjunto<T>::operator*(const Conjunto<T>&b) const{
-    int tam = tamArray + b.tamArray;
+    int tam  = (tamArray<b.tamArray)? tamArray : b.tamArray;
+    int totElem = num_elementos + b.num_elementos;
+
     Conjunto<T> temp(tam);
     for(int i = 0;i<tamArray;i++)temp.insere(c[i]);
     for(int i = 0;i<b.tamArray;i++) temp.insere(b.c[i]);
     Conjunto<T> temp2(tam);
 
-    for(int i=0;i<tam;i++){
+    for(int i=0;i<totElem;i++){
         if(pertence(temp.c[i]) && b.pertence(temp.c[i]))
             temp2.insere(c[i]);
     }
     return temp2;
 }
 
+
 template<class T>
 Conjunto<T> Conjunto<T>::operator-(const Conjunto<T>&b) const{
     int tam = tamArray + b.tamArray;
+    int totElem = num_elementos + b.num_elementos;
     Conjunto<T> temp(tam);
     for(int i = 0;i<tamArray;i++)temp.insere(c[i]);
     for(int i = 0;i<b.tamArray;i++) temp.insere(b.c[i]);
     Conjunto<T> temp2(tam);
 
-    for(int i=0;i<tam;i++){
+    for(int i=0;i<totElem;i++){
         if(pertence(temp.c[i]) && b.pertence(temp.c[i]))
             temp2.insere(c[i]);
     }
     Conjunto<T> temp3(tam);
-    for(int i=0;i<tam;i++){
+    for(int i=0;i<totElem;i++){
         if(b.pertence(temp.c[i]) && temp2.pertence(temp.c[i])){
             continue;
-        } else {
+        } else if(pertence(temp.c[i])){
             temp3.insere(c[i]);
         }
             
@@ -170,9 +173,15 @@ Conjunto<T> Conjunto<T>::operator-(const Conjunto<T>&b) const{
 
 template<class T>
 void Conjunto<T>::imprime() const {
-    cout << "{";
-    for(int i=0; i<num_elementos-1; i++) cout << c[i] << ", ";
-    cout << c[num_elementos-1]<< "}"<< endl;
+    cout << "{";  
+    for(int i=0; i<num_elementos-1; i++) cout << c[i] << ",";
+    if(num_elementos > 0){
+        cout << c[num_elementos-1]<< "}";
+    }else
+    {
+        cout << "}";
+    }
+    
 }
 
 template<class T>
@@ -181,17 +190,13 @@ ostream & operator<<(ostream &os , const Conjunto<T> &v){
     return os;
 }
 
-template<class T>
-void Conjunto<T>::le(Conjunto<T> v) {
-	  for(int i=0; i<v.tamArray; i++){
-            cin >> v.c[i];
-            insere(v.c[i]);
-      }									
-}
 
 template<class T>
 std::istream & operator>>(std::istream & in, Conjunto<T>& v){
-    v.le(v);
+    T aux;
+    while(in >> aux){
+        v.insere(aux);
+    }
     return in;
 }
 
