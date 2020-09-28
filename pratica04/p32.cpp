f8db78b27c5107384e9fcfcdac56bb049f605597
matheusximeness <matheus.ximenes2016@outlook.com>
2020-09-28 14:58:32 -0300

    etapa 3 finish

---
+++ pratica04/p32.cpp
@@ -0,0 +1,19 @@
+#include "Conjunto2.h"
+#include <iostream>
+
+
+int main(){
+    int n;
+    cout << "digite o tamanho dos vetores" << "\n";
+    cin >> n;
+
+    Conjunto<int> A(n), B(n), C;
+    for(int i=0;i<n;i++){
+        A.insere(i);
+        B.insere(-i);
+    }
+    C = A + B;
+    cout << C.pertence(10) << "\n";
+
+    return 0;
+}
