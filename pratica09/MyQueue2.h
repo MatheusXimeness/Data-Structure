5b2c03ebca7a3691adcdd9e1e429e236b60393e1
matheusximeness <matheus.ximenes2016@outlook.com>
2020-11-09 16:39:21 -0300

    Finish

--- pratica09/MyQueue2.h
+++ pratica09/MyQueue2.h
@@ -18,21 +18,48 @@ public:
 	int size() ;
 
 private:
+	void stackToQueue(); 
 	MyStack<T> pilha1; //nao mude nada nessa interface!!!
 	MyStack<T> pilha2; //nao mude nada nessa interface!!!
 };
 
 
-/* Faca sua implementacao abaixo desta linha...*/
-/* Faca sua implementacao abaixo desta linha...*/
-/* Faca sua implementacao abaixo desta linha...*/
-/* Faca sua implementacao abaixo desta linha...*/
+template<class T>
+void MyQueue2<T>::push(const T&el){
+	pilha1.push(el);
+}
 
+template<class T>
+void MyQueue2<T>::stackToQueue(){
+		while(!pilha1.empty()){
+			pilha2.push(pilha1.top());
+			pilha1.pop();
+		}
+}
 
+template<class T>
+const T MyQueue2<T>::front(){ //Implementação retornando por copia
+	if(pilha2.empty())
+		stackToQueue();
+	return pilha2.top();
+} 
 
+template<class T>
+void MyQueue2<T>::pop(){
+	if(pilha2.empty())
+		stackToQueue();
+	pilha2.pop();
+}
 
+template<class T>
+bool MyQueue2<T>::empty(){
+	return pilha1.empty() && pilha2.empty();
+}
 
-
+template<class T>
+int MyQueue2<T>::size(){
+	return pilha1.size()+pilha2.size();
+}
 
 
 #endif
--- pratica09/driver.cpp
+++ pratica09/driver.cpp
@@ -6,62 +6,63 @@ using namespace std;
 // Insira aqui o codigo para a etapa 1....
 
 //Voce pode adicionar includes como o abaixo...
-//#include "MyStack.h"
 #include "MyStack.h"
 #include "MyQueue.h"
 
 
 void etapa1() {
-	int n = 6;
-	MyStack<int> pilha;
-	MyQueue<int> fila;
-	for(int i=0;i<n;i++){
-		int value; cin >> value;
-		pilha.push(value);
-		fila.push(value);
-	}
+	MyStack<int> stack;
+	MyQueue<int> queue;
+	int elm;
 
-	while(!pilha.empty()){
-		cout << pilha.top() << " ";
-		pilha.pop();
+	for(int i = 0; i < 6; i++){
+		cin>>elm;
+		queue.push(elm);
+		stack.push(elm);
 	}
-	cout << '\n';
+	
+	while(!stack.empty()){
+		cout << stack.top() << " ";
+		stack.pop();
+	}
+	cout << endl;
 
-	while(!fila.empty()){
-		cout << fila.front() << " ";
-		fila.pop();
+	while(!queue.empty()){
+		cout << queue.front() << " ";
+		queue.pop();
 	}
-	cout << '\n';
+	cout << endl;	
+	
 }
+
+// Para a pilha os ultimos elementos a serem inseridos são os primeiros a serem impressos, enquanto para fila os primeiros a serem impressos 
+// são os primeiros elementos inseridos.
 
 //---------------------------------------
 
 // Insira aqui o codigo para a etapa 2....
 
 void etapa2() {
-	MyStack<char> pilha;
-	char symbol;
-	while(cin>>symbol){
-		if(symbol=='(' || symbol=='[' || symbol=='{')
-			pilha.push(symbol);
-			
-		if(pilha.empty() && (symbol==')' || symbol==']' || symbol=='}')){
-			cout << "Inconsistente\n"; return;
+	MyStack<char> stack;
+	char cons;
+	while(cin>>cons){
+		if(stack.empty() && (cons == ')' || cons == ']' || cons == '}')){
+			stack.push(cons);
+			break;
 		}
-
-		if(symbol==')' && pilha.top()=='(')
-			pilha.pop();
-		else if(symbol==']' && pilha.top()=='[')
-			pilha.pop();
-		else if(symbol=='}' && pilha.top()=='{')
-			pilha.pop();
-		
-		
+		if((cons == '(') || (cons == '[') || (cons == '{'))
+			stack.push(cons);
+		if((stack.top() == '(') && (cons == ')'))
+			stack.pop();
+		else if((stack.top() == '[') && (cons == ']'))
+			stack.pop();
+		else if((stack.top() == '{') && (cons == '}'))
+			stack.pop();
 	}
-	if(pilha.empty())
-		cout << "Consistente\n";
+	if(stack.empty())
+		cout<<"Consistente" << endl;
 	else
-		cout << "Inconsistente\n";
+		cout<<"Inconsistente" << endl;
 }
 
 //---------------------------------------
@@ -69,17 +70,9 @@ void etapa2() {
 // Na etapa 3 voce deve implementar MyQueue2.h e descomentar os codigos de teste abaixo...
 
 
-//#include "MyQueue2.h"
-#include "MyPriorityQueue.h"
+#include "MyQueue2.h"
 void etapa3() {
-	MyPriorityQueue<int> fila;
-	int n; cin >> n;
-	while(cin>>n){
-		fila.push(n);
-	}
-		fila.push(n);
-	fila.print();
-	/*char operacao;
+	char operacao;
 	MyQueue2<string> q;
 
 	//le um arquivo contendo "comandos" para a fila e aplica-os a fila criada com sua classe...
@@ -95,15 +88,36 @@ void etapa3() {
 		} else if(operacao=='e') {
 			cout << ((q.empty())?"vazia":"cheia") << "\n";
 		} else cout << q.size() << "\n";
-	}	*/
+	}	
 }
 
 //---------------------------------------
 
 // Insira aqui o codigo para a etapa 4....
 
-#include "MyPriorityQueue.h"
+
 void etapa4() {
+	int n, elm, cont;
+	cont = 1;
+	cin >> n;
+	MyStack<int> stack;
+	for (int i = 0; i < n; i++){
+		cin>> elm;
+		if(stack.empty() || elm > stack.top() ){
+			for(int j = cont; j <= elm; j++){
+				stack.push(j);
+				cont++;
+			}
+			stack.pop();	
+		} else if(elm == stack.top()){
+				stack.pop();
+		} else
+			break;
+	}
+	if(stack.empty())
+		cout<< "SIM" << endl;
+	else
+		cout << "NAO" << endl; 
 	
 }
 
