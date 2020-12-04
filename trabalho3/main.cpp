f3e1b6d67a6d33e919eab97c5214728ceb5aec93
matheusximeness <matheus.ximenes2016@outlook.com>
2020-12-04 18:06:28 -0300

    Roteiro

---
+++ trabalho3/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string.h>
+#include "MyMap.h"
+
+
+using namespace std;
+
+int main(){
+
+        MyMap<string,int> map;
+        string str;
+        int tam = 0;
+
+        while(cin>>str){
+                map.insert(pair<string,int> (str, tam));
+                cout << str << " " << map[str] << endl;
+                tam = map.size();
+        }
+}
